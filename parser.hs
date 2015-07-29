910a202428308740a918a1585063a6bf9cd1bceb
Masaki Waga <masakiwaga@gmail.com>
2015-07-30 08:27:44 +0900

    modified some

--- parser.hs
+++ parser.hs
@@ -18,6 +18,9 @@ isAccepting node = or $ Prelude.map (sameAttribute acceptingAttribute) attribute
     where
       attributes = nodeAttributes node 
 
+isStart :: State -> Bool
+isStart node = nodeID node == "start"
+           
 textToGraph :: Data.Text.Lazy.Text -> AutoMata
 textToGraph txt = parseDotGraph txt
 
@@ -30,14 +33,21 @@ findLasso automata = Prelude.map nodeID $ L.foldl' (\ li root -> if li == [] the
       -- Depth First Search
       searchLasso :: State -> State -> [State] -> [State]
       searchLasso root now hist 
-          | root == now && hist /=[] =  L.reverse $ now:hist
-          | elem now hist =  []
+          | root == now && hist /= [] =  L.reverse $ now:hist
+          | elem now hist  =  []
           | otherwise      = L.foldl' (\ l s -> 
                                            if l /= [] then
                                                l
                                            else
                                                searchLasso root s (now:hist)
                                       ) [] $ nextStates ! now
+      traceStates :: [State] -> State -> [State]
+      traceStates hist now 
+          | searchNext == [] = hist
+          | otherwise = L.foldl' traceStates (now:hist) searchNext
+          where
+            nexts = nextStates ! now
+            searchNext = Prelude.filter (\ s -> notElem s hist) nexts
       nextStates :: Map State [State]
       nextStates = fromList nextStatesList
       nextStatesList = [(from,[to |
@@ -46,7 +56,9 @@ findLasso automata = Prelude.map nodeID $ L.foldl' (\ li root -> if li == [] the
                                fromNode t == nodeID from,
                                nodeID to == toNode t])|
                         from <- states]
-      acceptingStates = Prelude.filter isAccepting states
+      acceptingStates = Prelude.filter isAccepting reachableStates
+      startState = L.head $ Prelude.filter isStart states
+      reachableStates = traceStates [startState] startState
       states = graphNodes automata
       transitions = graphEdges automata
 
@@ -57,28 +69,3 @@ main = do
         putStrLn "There is no accepting path."
     else
         putStrLn $ "Found accepting path:" ++ (show lasso) 
-
-sampleAutomata = "\
-\digraph finite_state_machine {\
-\    rankdir=LR;\
-\    size=\"8,5\"\
-\    LR_0 [shape = doublecircle];\
-\    LR_3 [shape = doublecircle];\
-\    LR_4 [shape = doublecircle];\
-\    LR_8 [shape = doublecircle];\
-\    LR_0 -> LR_2 [ label = \"SS(B)\" ];\
-\    LR_0 -> LR_1 [ label = \"SS(S)\" ];\
-\    LR_1 -> LR_3 [ label = \"S($end)\" ];\
-\    LR_2 -> LR_6 [ label = \"SS(b)\" ];\
-\    LR_2 -> LR_5 [ label = \"SS(a)\" ];\
-\    LR_2 -> LR_4 [ label = \"S(A)\" ];\
-\    LR_5 -> LR_7 [ label = \"S(b)\" ];\
-\    LR_5 -> LR_5 [ label = \"S(a)\" ];\
-\    LR_6 -> LR_6 [ label = \"S(b)\" ];\
-\    LR_6 -> LR_5 [ label = \"S(a)\" ];\
-\    LR_7 -> LR_8 [ label = \"S(b)\" ];\
-\    LR_7 -> LR_5 [ label = \"S(a)\" ];\
-\    LR_8 -> LR_6 [ label = \"S(b)\" ];\
-\    LR_8 -> LR_5 [ label = \"S(a)\" ];\
-\}\
-\"
